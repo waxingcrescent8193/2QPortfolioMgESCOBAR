f5421e1d3a640ca37b29a7ebb4d780ade6f58214
waxingcrescent8193 <b2029jezescobar@pshs.edu.ph>
2025-10-13 12:20:38 +0800

    graded task1 buttons now work

--- public/graded1.js
+++ public/graded1.js
@@ -1,9 +1,27 @@
-function tutorApplication() {
-    
+let tutorForm = document.getElementById("tutorApplicationForm");
+let studentForm = document.getElementById("studentApplicationForm");
+
+
+function tutorApplication() {  
+    if (studentForm.style.display === "block")
+        studentForm.style.display = "none";
+
+    if (tutorForm.style.display === "block") 
+        tutorForm.style.display = "none"; 
+
+    else 
+        tutorForm.style.display = "block";
 }
 
 function studentApplication () {
-            
+    if (tutorForm.style.display === "block")
+        tutorForm.style.display = "none";
+
+    if (studentForm.style.display === "block") 
+        studentForm.style.display = "none"; 
+
+    else 
+        studentForm.style.display = "block";
 }
 
 function submittedTutorApplication() {
